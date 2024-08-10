7c32fbcfdcbed49a7ace5cc891ba968345c79097
shaswat931 <shaswatshukla931@gmail.com>
2024-08-11 00:25:19 +0530

    ok

---
+++ Nexus/home.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("defaultOpen").click();
+});
+
+function openForm(evt, formName) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(formName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
